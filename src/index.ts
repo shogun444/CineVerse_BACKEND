1cc606fcf4be1a756ab427b18450441f6c0d43d0
Sayandip Roy <161803450+shogun444@users.noreply.github.com>
2025-11-02 10:42:34 +0530

    Update CORS origin to production URL

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({origin : "http://localhost:3000"}))
+app.use(cors({origin : "https://king-flix.vercel.app/"}))
 
 app.get('/', (req, res) => {
   res.status(200).json({ msg: "HealthCheck : Good" });
